554c83295efbe1e563cde31ff9063fc882de6123
vplauzon <vincent_lauzon@yahoo.com.au>
2022-03-30 12:03:55 -0400

    Add config file

---
+++ code/ParquetQueryTest/StorageQueryConsole/Config/RootConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageQueryConsole.Config
+{
+    internal class RootConfiguration
+    {
+        public string? Endpoint { get; set; }
+    }
+}
---
+++ code/ParquetQueryTest/StorageQueryConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using StorageQueryConsole.Config;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace StorageQueryConsole
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //ServicePointManager.DefaultConnectionLimit = 25;
+
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("You have to call this exec with a parameter pointing to the config file");
+            }
+            else
+            {
+                var config = await LoadConfigAsync(args);
+            }
+        }
+
+        private static async Task<RootConfiguration> LoadConfigAsync(string[] args)
+        {
+            var configPath = args[0];
+            var configContent = await File.ReadAllTextAsync(configPath);
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+            var config = deserializer.Deserialize<RootConfiguration>(configContent);
+
+            return config;
+        }
+    }
+}
